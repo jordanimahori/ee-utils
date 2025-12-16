16b95fccac30cc3f8c2dc4730c31918182d3cce5
Jordan Imahori <jordanimahori@gmail.com>
2025-12-16 03:11:32 -0500

    Improvements to image exporting utils to flexibly export image patches along a grid

--- ee_utils.py
+++ ee_utils.py
@@ -1,4 +1,4 @@
-import ee, requests, numpy as np
+import ee, requests, re, numpy as np, pandas as pd
 from matplotlib import pyplot as plt
 from pyproj import Transformer
 from io import BytesIO
@@ -27,23 +27,30 @@ def wgs84_to_utm(pt: tuple[float, float], crs_epsg: str) -> tuple[float, float]:
 
 def get_patch(image: ee.Image,
               pt: tuple[float, float],
-              scale: int | tuple[int, int],
-              crs_epsg: str | None = None,
+              pt_crs: str = "EPSG:4326",
+              patch_scale: int | tuple[int, int] = 30,
+              patch_crs: str | None = None,
               patch_size: int = 256,
               add_x_offset: int = 0,
               add_y_offset: int = 0,
               file_format: str = "NUMPY_NDARRAY"):
 
-    # Get patch center in CRS coords
-    if crs_epsg is None:
-        crs_epsg = get_utm_epsg(pt)
-    centroid = wgs84_to_utm(pt, crs_epsg=crs_epsg)
+    # Get patch center in UTM coords; assume WGS84 if none supplied
+    if patch_crs is None:
+        assert pt_crs.lower() == "epsg:4326"
+        patch_crs = get_utm_epsg(pt)
+        centroid = wgs84_to_utm(pt, crs_epsg=patch_crs)
+    elif re.fullmatch(r"(?:epsg:)?(326|327)\d{2}", patch_crs.lower()):
+        assert pt_crs.lower() == patch_crs.lower()
+        centroid = pt
+    else:
+        raise NotImplementedError
 
-    # scale unpack
-    if isinstance(scale, int):
-        scale_x, scale_y = scale, -scale
-    elif isinstance(scale, tuple) and len(scale) == 2:
-        scale_x, scale_y = scale[0], -scale[1]
+    # patch_scale unpack
+    if isinstance(patch_scale, int):
+        scale_x, scale_y = patch_scale, -patch_scale
+    elif isinstance(patch_scale, tuple) and len(patch_scale) == 2:
+        scale_x, scale_y = patch_scale[0], -patch_scale[1]
         assert isinstance(scale_x, int) and isinstance(scale_y, int)
     else:
         raise TypeError("Scale must be an integer or tuple of two integers")
@@ -62,7 +69,7 @@ def get_patch(image: ee.Image,
                 'scaleX': scale_x, 'shearX': 0, 'translateX': tx,
                 'shearY': 0,      'scaleY': scale_y, 'translateY': ty
             },
-            'crsCode': crs_epsg
+            'crsCode': patch_crs
         }
     }
     try:
@@ -73,8 +80,9 @@ def get_patch(image: ee.Image,
 
 def get_neighbourhood(image: ee.Image,
                       pt: tuple[float, float],
-                      scale: int,
-                      crs_epsg: str,
+                      patch_scale: int,
+                      patch_crs: str,
+                      pt_crs: str = "EPSG:4326",
                       patch_size: int = 256,
                       levels: int = 2,
                       file_format: str = "NUMPY_NDARRAY"):
@@ -84,7 +92,7 @@ def get_neighbourhood(image: ee.Image,
 
     def fetch_patch(offset):
         x, y = offset
-        return (x, y), get_patch(image=image, pt=pt, crs_epsg=crs_epsg, scale=scale, patch_size=patch_size,
+        return (x, y), get_patch(image=image, pt=pt, pt_crs=pt_crs, patch_crs=patch_crs, patch_scale=patch_scale, patch_size=patch_size,
                                  add_x_offset=x*patch_size, add_y_offset=y*patch_size, file_format=file_format)
 
     # Get patches at given offset relative to centroid for each x,y tuple
@@ -94,7 +102,7 @@ def get_neighbourhood(image: ee.Image,
     return dict(results)
 
 
-def plot_neighbourhood(patches: dict[tuple[int,int], np.ndarray|dict],
+def plot_neighbourhood(patches: dict[tuple[int,int], np.ndarray],
                        levels: int,
                        bands: str | list[str],
                        vis_min: float,
@@ -183,16 +191,17 @@ def preview_patch(image: ee.Image,
     plt.show()
 
 
-def write_dataset(image: ee.Image,
-                  locations: list[list[float, float]],
-                  location_ids: list[str],
-                  image_names: list[str],
-                  patch_size: int,
-                  scale: int,
-                  output_dir: str ,
-                  overwrite_patches: bool = True,
-                  concurrent_requests: int = 40,
-                  ) -> None:
+def extract_patches(image: ee.Image,
+                    output_dir: str,
+                    pts: list[tuple[float, float]],
+                    patch_names: list[str],
+                    patch_size: int,
+                    patch_scale: int,
+                    patch_crs: str | None = None,
+                    pts_crs: str = "EPSG:4326",
+                    overwrite_patches: bool = True,
+                    concurrent_requests: int = 40,
+                    ) -> None:
     """
     Extract patches from Earth Engine and save as a GeoTIFF.
 
@@ -201,16 +210,16 @@ def write_dataset(image: ee.Image,
     """
 
     # Check inputs
-    assert len(locations) == len(location_ids)
+    assert len(pts) == len(patch_names)
+    assert len(patch_names) == len(set(patch_names))
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     # Skip existing patches if names match
-    if overwrite_patches is False:
-        existing_patches = list(Path(output_dir).glob("*.tif"))
-        skip_ids = [pt_id for pt_id, img_name in zip(location_ids, image_names)
-                    if Path(output_dir).joinpath(img_name) in existing_patches]
+    if not overwrite_patches:
+        existing_names = {p.stem for p in Path(output_dir).glob("*.tif")}
+        skip_ids = [n for n in patch_names if n in existing_names]
         # and return early if all IDs to be skipped
-        if len(skip_ids) == len(location_ids):
+        if len(skip_ids) == len(patch_names):
             print("...all patches already exist.")
             return None
         if len(skip_ids) > 0:
@@ -220,22 +229,20 @@ def write_dataset(image: ee.Image,
 
     future_to_point = {}
     with concurrent.futures.ThreadPoolExecutor(max_workers=concurrent_requests) as executor:
-
-        # Submit jobs
-        for pt, pt_id, img_name in zip(locations, location_ids, image_names):
+        for pt, pt_id in zip(pts, patch_names):
             if pt_id not in skip_ids:
-                future = executor.submit(get_patch, image=image, pt=pt, scale=scale,
-                                         patch_size=patch_size, file_format='GEOTIFF')
-                future_to_point[future] = (pt_id, img_name)
+                future = executor.submit(get_patch, image=image, pt=pt, pt_crs=pts_crs, patch_scale=patch_scale,
+                                         patch_size=patch_size, patch_crs=patch_crs, file_format='GEO_TIFF')
+                future_to_point[future] = pt_id
 
         # Write patches to disk when available
         progbar = tqdm(total=len(future_to_point))
         for future in concurrent.futures.as_completed(future_to_point):
-            pt_id, img_name = future_to_point[future]
+            pt_id = future_to_point[future]
 
             try:
                 img = future.result()
-                filepath = Path(output_dir).joinpath(img_name)
+                filepath = Path(output_dir).joinpath(f"{pt_id}.tif")
                 with open(filepath, 'wb') as writer:
                     writer.write(img)
 
@@ -243,11 +250,65 @@ def write_dataset(image: ee.Image,
                 print(e)
 
             finally:
-                # remove patch from dict to minimise memory utilisation
                 future_to_point.pop(future)
                 progbar.update(1)
 
         progbar.close()
+
+
+def extract_spatial_covariates(image: ee.Image,
+                               pts: list[tuple[float, float]],
+                               patch_ids: list[str],
+                               patch_scale: int,
+                               patch_crs: str | None = None,
+                               pt_crs: str = "EPSG:4326",
+                               concurrent_requests: int = 40,
+                               **kwargs
+                               ) -> pd.DataFrame:
+    """
+    Extract average covariate values for a list of points and save as a Pandas DataFrame.
+
+    In case this runs too fast and some requests hit EarthEngine's API rate limits, re-run the export with
+    overwrite_patches = False to only download missing patches.
+    """
+
+    assert len(pts) == len(patch_ids)
+
+    future_to_point = {}
+    patch_dfs = []
+    errors = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=concurrent_requests) as executor:
+
+        # Submit jobs
+        for pt, pt_id in zip(pts, patch_ids):
+                future = executor.submit(get_patch, image=image, pt=pt, pt_crs=pt_crs, patch_scale=patch_scale,
+                                         patch_size=1, patch_crs=patch_crs, **kwargs)
+                future_to_point[future] = pt_id
+
+        # Write patches to disk when available
+        progbar = tqdm(total=len(future_to_point))
+        for future in concurrent.futures.as_completed(future_to_point):
+            pt_id = future_to_point[future]
+
+            try:
+                img = future.result()
+                df = pd.DataFrame(img.reshape(-1))
+                df["dhs_id"] = pt_id
+                patch_dfs.append(df)
+
+            except Exception as e:
+                print(f"Encountered error for {pt_id}:")
+                errors.append(pt_id)
+                print(e)
+
+            finally:
+                future_to_point.pop(future)
+                progbar.update(1)
+
+        progbar.close()
+
+    return pd.concat(patch_dfs), errors
+
 
 
 def prefix_bands(img, prefix):
@@ -494,7 +555,7 @@ class Sentinel2SR:
           linked by system:index, using qa_band >= clear_threshold.
 
         Args:
-            bands: Optional band subset (after renaming); e.g. ['RED','GREEN','BLUE'].
+            bands: Optional band subset (e.g. ['B2', 'B3', 'B4', 'B8']).
             rescale: Multiply reflectance by 1e-4 to get [0..1] floats.
             qa_band: 'cs' or 'cs_cdf' (recommended: 'cs_cdf').
             clear_threshold: keep pixels with qa_band >= this value.
